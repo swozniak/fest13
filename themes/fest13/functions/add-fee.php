a4a773f123f9ce280249a27b6912cc137001cb00
PJ Fancher <peej@noidearecords.com>
2014-04-16 00:16:58 -0400

    Fixed processing fee for international orders

--- themes/fest13/functions/add-fee.php
+++ themes/fest13/functions/add-fee.php
@@ -2,8 +2,14 @@
 
 	add_filter( 'woocommerce_calc_tax', 'add_fee_to_tax' );
 	function add_fee_to_tax( $taxes ) {
+		 //USA
 		if( $taxes[11] ) {
 			$taxes[11] = $taxes[11] + 2.5;
+		}
+
+		//International
+		if( $taxes[12] ) {
+			$taxes[12] = $taxes[12] + 2.5;
 		}
 
 		 return $taxes;

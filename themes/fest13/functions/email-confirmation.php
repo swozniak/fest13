e821bd7ea6e9aafa2052a8ec61c9e5828b985c3c
PJ Fancher <peej@noidearecords.com>
2014-04-22 21:58:13 -0400

    uncomment

--- themes/fest13/functions/email-confirmation.php
+++ themes/fest13/functions/email-confirmation.php
@@ -1,54 +1,54 @@
 <?php
 	//SCRIPT TO SEND OUT EMAILS TO ALL PROCESSING ORDERS WHO DID NOT ORDER FROM THE HOLIDAY INN
     //Load Wordpress
-	//$parse_uri = explode( 'wp-content', __FILE__ );
-	//$public_html = $parse_uri[0];
-	//require_once( $public_html . 'wp-load.php' );
-	//global $woocommerce;
-	//$hi_ids = array( 892, 894, 893 );
-	//$order_count = wp_count_posts( 'shop_order' );
-	//$orders_at_a_time = 50;
-	//$sent = 0;
-	//$log_file = get_stylesheet_directory() . '/functions/logs/email-confirmation-log-' . mktime() . '.log';
-	//$fh = fopen( $log_file, 'w' );
+	$parse_uri = explode( 'wp-content', __FILE__ );
+	$public_html = $parse_uri[0];
+	require_once( $public_html . 'wp-load.php' );
+	global $woocommerce;
+	$hi_ids = array( 892, 894, 893 );
+	$order_count = wp_count_posts( 'shop_order' );
+	$orders_at_a_time = 50;
+	$sent = 0;
+	$log_file = get_stylesheet_directory() . '/functions/logs/email-confirmation-log-' . mktime() . '.log';
+	$fh = fopen( $log_file, 'w' );
 
-	//for( $offset = 0; $offset <= $order_count->publish; $offset += $orders_at_a_time ) :
-		//$args = array(
-			//'post_type' => 'shop_order',
-			//'posts_per_page' => $orders_at_a_time,
-			//'offset' => $offset,
-		//);
+	for( $offset = 0; $offset <= $order_count->publish; $offset += $orders_at_a_time ) :
+		$args = array(
+			'post_type' => 'shop_order',
+			'posts_per_page' => $orders_at_a_time,
+			'offset' => $offset,
+		);
 
-		//$query = new WP_Query( $args );
+		$query = new WP_Query( $args );
 
-		//while( $query->have_posts() ) :  $query->the_post();
-			//$order_id = get_the_ID();
-			//$order = new WC_Order();
-			//$order->get_order( $order_id );
-			//$items = $order->get_items();
-			//$send_email = true;
+		while( $query->have_posts() ) :  $query->the_post();
+			$order_id = get_the_ID();
+			$order = new WC_Order();
+			$order->get_order( $order_id );
+			$items = $order->get_items();
+			$send_email = true;
 
-			//if( $order->status != 'processing' ) :
-				//$send_email = false;
-			//endif;
+			if( $order->status != 'processing' ) :
+				$send_email = false;
+			endif;
 
-			//foreach( $items as $item ) :
-				//if( in_array( $item['product_id'], $hi_ids ) ) :
-					//$send_email = false;
-				//endif;
-			//endforeach;
+			foreach( $items as $item ) :
+				if( in_array( $item['product_id'], $hi_ids ) ) :
+					$send_email = false;
+				endif;
+			endforeach;
 
-			//if( $send_email ) :
-				//$sent++;
-				//fwrite( $fh, "Sending email for order $order_id\n" );
-				////$mailer = $woocommerce->mailer();
-				////$email = $mailer->emails['WC_Email_Customer_Processing_Order'];
-				////$email->trigger( $order_id );
-				//fwrite( $fh, "Email sent for order $order_id\n\n" );
-			//endif;
-		//endwhile;
-		////sleep( 5 );
-	//endfor;
-	//fwrite( $fh, "$sent total emails sent\n" );
-	//fclose( $fh );
+			if( $send_email ) :
+				$sent++;
+				fwrite( $fh, "Sending email for order $order_id\n" );
+				//$mailer = $woocommerce->mailer();
+				//$email = $mailer->emails['WC_Email_Customer_Processing_Order'];
+				//$email->trigger( $order_id );
+				fwrite( $fh, "Email sent for order $order_id\n\n" );
+			endif;
+		endwhile;
+		//sleep( 5 );
+	endfor;
+	fwrite( $fh, "$sent total emails sent\n" );
+	fclose( $fh );
 ?>

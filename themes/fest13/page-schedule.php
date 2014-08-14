0d907032af4e767b1448fb5d95154198c3cf6641
Steve Wozniak <stephan.wozniak@gmail.com>
2014-08-13 20:03:00 -0400

    adding time sort

--- themes/fest13/page-schedule.php
+++ themes/fest13/page-schedule.php
@@ -36,11 +36,13 @@ function get_date_label( $day ) {
 	return $label;
 }
 
-$venues_url = 'http://localhost/api/v1/venues';
+$protocol = ( !empty( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] !== 'off') ? 'https://' : 'http://';
+
+$venues_url = $protocol . $_SERVER['HTTP_HOST'] . '/api/v1/venues';
 $venues_response = file_get_contents( $venues_url );
 $venues_array = json_decode( $venues_response, true );
 
-$events_url = 'http://localhost/api/v1/events';
+$events_url = $protocol . $_SERVER['HTTP_HOST'] . '/api/v1/events';
 $events_response = file_get_contents( $events_url );
 $events_array = json_decode( $events_response, true );
 
@@ -49,13 +51,15 @@ $schedule_days = ( is_prefest() ) ? array( 1, 2 ) : array( 3, 4, 5 );
 
 $schedule_events = array();
 
-if ( $sort_method !== 'time' ) {
-	foreach ( $events_array as $event ) {
-		if ( ( $event['venue']['nickname'] !== 'Events (API)' ) && ( in_array( $event['day'], $schedule_days, false ) ) )	 { 
+foreach ( $events_array as $event ) {
+	if ( ( $event['venue']['nickname'] !== 'Events (API)' ) && ( in_array( $event['day'], $schedule_days, false ) ) )	 { 
+		if ( $sort_method !== 'time' ) {
 			if ( is_null( $schedule_events[$event['day']][$event['venue']['nickname_id']] ) ) {
 				$schedule_events[$event['day']][$event['venue']['nickname_id']] = $event['venue'];
 			}
 			$schedule_events[$event['day']][$event['venue']['nickname_id']]['events'][] = $event;
+		} else {
+			$schedule_events[$event['day']]['events'][] = $event;
 		}
 	}
 }
@@ -74,71 +78,131 @@ if ( $sort_method !== 'time' ) {
 	<?php foreach ( $schedule_days as $day ) : ?>
 		<div class="col-xs-12">
 			<h3 class="schedule-date-label"><?php echo get_date_label( $day ); ?></h3><br />
-			<?php $count = 0; ?>
-			<?php foreach ( $schedule_events[$day] as $venue ) : ?>
-				<?php 
-				$venue_info = $venues_array[$venue['id']];
-				$venue_meta = get_post_custom( $venue['nickname_id'] );
-				?>
-				<?php if ( $count == 0) : ?>
-					<?php echo '<div class="row">'; ?>
-					<div class="col-xs-12 col-sm-5 schedule-venue-container">
-				<?php else : ?>
-					<div class="col-xs-12 col-xs-offset-0 col-sm-5 col-sm-offset-1 schedule-venue-container">
-				<?php endif; ?>
-					<h2 class="schedule-venue-nickname"><?php echo $venue['nickname']; ?></h2><br />
-					<h3 class="schedule-venue-address"><?php echo $venue_info['address']; ?></h3>
-					<?php usort( $venue['events'], 'time_sort' ); ?>
 
-					<table>
-					<?php foreach ( $venue['events'] as $event ) : ?>
-						<tr>
-							<td style="width:160px">
-								<?php
-								echo $event['start_string'];
-
-								if ( $event['end_string'] ) {
-									echo ' - ' . $event['end_string'];
-								}
-								?>
-							</td>
-							<td>
-								<?php
-								if ( !is_null( $event['band'] ) ) {
-
-									if ( !is_null( $event['band']['url'] ) ) {
-										printf( '<a href="%s">%s</a>', $event['band']['url'], $event['band']['name'] );
-									} else {
-										echo $event['band']['name'];
-									}
-
-									if ( !is_null( $event['memo'] ) && strlen( $event['memo'] ) > 0 ) {
-										echo ' (' . $event['memo'] . ')';
-									}
-								} else {
-									if ( !is_null( $event['memo'] ) && strlen( $event['memo'] ) > 0 ) {
-										echo $event['memo'];
-									}
-								}
-								?>
-							</td>	
-						</tr>
-					<?php endforeach; ?>
-					</table>
-					<hr />
+			<?php if ( $sort_method !== 'time' ) : ?>
+				<?php $count = 0; ?>
+				<?php foreach ( $schedule_events[$day] as $venue ) : ?>
 					<?php 
-					$notes = $venue_meta[implode( '-', array( 'wpcf-venue-day',  $day, 'notes' ) )][0];
-					echo $notes . '<br /><br />';
+					$venue_info = $venues_array[$venue['id']];
+					$venue_meta = get_post_custom( $venue['nickname_id'] );
 					?>
-				</div>
+					<?php if ( $count == 0 ) : ?>
+						<?php echo '<div class="row">'; ?>
+						<div class="col-xs-12 col-sm-5 schedule-venue-container">
+					<?php else : ?>
+						<div class="col-xs-12 col-xs-offset-0 col-sm-5 col-sm-offset-1 schedule-venue-container">
+					<?php endif; ?>
+						<h2 class="schedule-venue-nickname"><?php echo $venue['nickname']; ?></h2><br />
+						<h3 class="schedule-venue-address"><?php echo $venue_info['address']; ?></h3>
+						<?php usort( $venue['events'], 'time_sort' ); ?>
 
-				<?php if ( $count == 1 ) {
-					echo '</div>';
-					$count = 0;
-				} else {
-					$count = 1;
-				} ?>
-			<?php endforeach; ?>
+						<table>
+						<?php foreach ( $venue['events'] as $event ) : ?>
+							<tr>
+								<td style="width:160px">
+									<?php
+									echo $event['start_string'];
+
+									if ( $event['end_string'] ) {
+										echo ' - ' . $event['end_string'];
+									}
+									?>
+								</td>
+								<td>
+									<?php
+									if ( !is_null( $event['band'] ) ) {
+
+										if ( !is_null( $event['band']['url'] ) ) {
+											printf( '<a href="%s">%s</a>', $event['band']['url'], $event['band']['name'] );
+										} else {
+											echo $event['band']['name'];
+										}
+
+										if ( !is_null( $event['memo'] ) && strlen( $event['memo'] ) > 0 ) {
+											echo ' (' . $event['memo'] . ')';
+										}
+									} else {
+										if ( !is_null( $event['memo'] ) && strlen( $event['memo'] ) > 0 ) {
+											echo $event['memo'];
+										}
+									}
+									?>
+								</td>	
+							</tr>
+						<?php endforeach; ?>
+						</table>
+						<hr />
+						<?php 
+						$notes = $venue_meta[implode( '-', array( 'wpcf-venue-day',  $day, 'notes' ) )][0];
+						echo $notes . '<br /><br />';
+						?>
+					</div>
+
+					<?php if ( $count == 1 ) {
+						echo '</div>';
+						$count = 0;
+					} else {
+						$count = 1;
+					} ?>
+				<?php endforeach; ?>
+			<?php else: ?>
+				<?php usort( $schedule_events[$day]['events'], 'time_sort' ); ?>
+				<table style="width:100%">
+					<thead>
+						<td style="width:33%">Time</td>
+						<td style="width:33%">Band</td>
+						<td style="width:33%">Venue</td>
+					</thead>
+				<?php foreach ( $schedule_events[$day]['events'] as $event ) : ?>
+					<?php 
+						$venue_info = $venues_array[$event['venue']['id']];
+						$venue_meta = get_post_custom( $event['venue']['nickname_id'] );
+					?>
+					<tr>
+						<td>
+							<?php
+							echo $event['start_string'];
+
+							if ( $event['end_string'] ) {
+								echo ' - ' . $event['end_string'];
+							}
+							?>
+						</td>
+						<td>
+							<?php
+							if ( !is_null( $event['band'] ) ) {
+
+								if ( !is_null( $event['band']['url'] ) ) {
+									printf( '<a href="%s">%s</a>', $event['band']['url'], $event['band']['name'] );
+								} else {
+									echo $event['band']['name'];
+								}
+
+								if ( !is_null( $event['memo'] ) && strlen( $event['memo'] ) > 0 ) {
+									echo ' (' . $event['memo'] . ')';
+								}
+							} else {
+								if ( !is_null( $event['memo'] ) && strlen( $event['memo'] ) > 0 ) {
+									echo $event['memo'];
+								}
+							}
+							?>
+						</td>	
+						<td>
+							<?php
+							if ( !is_null( $venue_info ) ) {
+								if ( !is_null( $venue_info['url'] ) ) {
+									printf( '<a href="%s">%s</a>', $venue_info['url'], $event['venue']['nickname'] );
+								} else {
+									echo $event['venue']['nickname'];
+								}
+							}
+							?>
+						</td>	
+					</tr>
+				<?php endforeach; ?>
+				</table>
+			<?php endif; ?>
 		</div>
 	<?php endforeach; ?>
 	</div><!-- .entry-content -->

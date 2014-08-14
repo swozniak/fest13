23bfa4947b233d85f1ca620758800cfc1b96e0c1
PJ Fancher <peej@noidearecords.com>
2014-08-13 22:44:07 -0400

    Started single day tickets

--- themes/fest13/template-parts/store/single-day-tickets.php
+++ themes/fest13/template-parts/store/single-day-tickets.php
@@ -15,18 +15,19 @@
 <div class="row">
 	<h2><?php echo $product_name; ?></h2>
 	<?php while( $products->have_posts() ) : $products->the_post(); ?>
-		<div class="col-xs-12 col-md-4">
+		<div class="col-xs-12 col-md-12">
 			<?php //publish_product( get_the_ID() ); ?>
 			<?php $id = get_the_ID(); ?>
+			<div class="description">
+				<div class="ticket-thumbnail">
+					<?php echo get_the_post_thumbnail( $id, array( 200, 200 ) ); ?>
+				</div><!-- end .ticket-thumbnail -->
+				<div class="content">
+					<?php echo get_the_content(); ?>
+				</div><!-- end .content -->
+			</div><!-- end .description -->
 			<?php echo do_shortcode("[product id=$id]"); ?>
+			
 		</div>
 	<?php endwhile; ?>
 </div>
-
-<?php 
-	if( $product_slug == 'ticket' ) :
-		$product_slug = 'single-day-ticket';
-		$product_name = 'Single Day Tickets';
-		include( locate_template( 'template-parts/store/single-day-tickets.php' ) );
-	endif; 
-?>

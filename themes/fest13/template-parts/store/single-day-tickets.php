6aa0adbd941e3e0d3b62bab5f3a3d36b1fb599c4
PJ Fancher <peej@noidearecords.com>
2014-08-14 20:50:59 -0400

    Finished up single day tickets

--- themes/fest13/template-parts/store/single-day-tickets.php
+++ themes/fest13/template-parts/store/single-day-tickets.php
@@ -11,23 +11,27 @@
 
 	$products = get_products( $args );
 ?>
- 
-<div class="row">
-	<h2><?php echo $product_name; ?></h2>
-	<?php while( $products->have_posts() ) : $products->the_post(); ?>
-		<div class="col-xs-12 col-md-12">
-			<?php //publish_product( get_the_ID() ); ?>
-			<?php $id = get_the_ID(); ?>
-			<div class="description">
-				<div class="ticket-thumbnail">
-					<?php echo get_the_post_thumbnail( $id, array( 200, 200 ) ); ?>
-				</div><!-- end .ticket-thumbnail -->
-				<div class="content">
-					<?php echo get_the_content(); ?>
-				</div><!-- end .content -->
-			</div><!-- end .description -->
-			<?php echo do_shortcode("[product id=$id]"); ?>
-			
+<?php if( $products->post_count ) : ?> 
+	<section class="single-day-tickets">
+		<div class="row">
+			<h2><?php echo $product_name; ?></h2>
+			<?php while( $products->have_posts() ) : $products->the_post(); ?>
+				<div class="col-xs-12 col-md-12">
+					<div class="single-day-ticket">
+						<?php //publish_product( get_the_ID() ); ?>
+						<?php $id = get_the_ID(); ?>
+						<div class="description">
+							<div class="ticket-thumbnail">
+								<?php echo get_the_post_thumbnail( $id, array( 300, 300 ) ); ?>
+							</div><!-- end .ticket-thumbnail -->
+							<div class="content">
+								<?php echo get_the_content(); ?>
+							</div><!-- end .content -->
+						</div><!-- end .description -->
+						<?php echo do_shortcode("[product id=$id]"); ?>
+					</div><!-- end .single-day-ticket -->
+				</div>
+			<?php endwhile; ?>
 		</div>
-	<?php endwhile; ?>
-</div>
+	</section>
+<?php endif; ?>

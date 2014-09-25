d1eabd49eca1b81c57c8986f7c62f030bea42c89
PJ Fancher <peej@noidearecords.com>
2014-09-24 22:33:16 -0400

    More reg css

--- themes/fest13/admin-css.php
+++ themes/fest13/admin-css.php
@@ -19,6 +19,15 @@ a.add-new-h2,
 #woocommerce-order-items .add_items,
 #woocommerce-order-items .bulk_actions {
   display: none; }
+#woocommerce-order-items .line_cost,
+#woocommerce-order-items .line_tax,
+#woocommerce-order-items .tax_class {
+  display: none; }
+#woocommerce-order-items .quantity {
+  font-size: 15px;
+  font-weight: bold; }
+#woocommerce-order-items .name a {
+  pointer-events: none; }
 
 #woocommerce-order-actions #actions {
   display: none; }
@@ -26,5 +35,66 @@ a.add-new-h2,
 #woocommerce-order-notes select {
   display: none; }
 
+.address a {
+  pointer-events: none; }
+
 .note.customer-note {
   display: block; }
+
+ul.subsubsub {
+  display: none; }
+
+p.search-box {
+  float: left;
+  margin: 10px 0; }
+
+.widefat td, .widefat th {
+  padding: 15px 10px; }
+
+#menu-posts-shop_order ul li:last-child {
+  display: none; }
+
+.widefat .column-order_status mark {
+  top: 10px; }
+
+.widefat .column-order_status mark.processing:after {
+  color: white;
+  background-color: #73a724;
+  border-radius: 50%; }
+
+.widefat .column-order_status mark.failed:after {
+  color: yellow;
+  background-color: red;
+  border-radius: 50%; }
+
+.widefat .column-order_status mark.refunded:after {
+  color: yellow;
+  background-color: red;
+  border-radius: 50%; }
+
+form#posts-filter .tablenav {
+  display: none; }
+form#posts-filter table.wp-list-table {
+  width: 200%; }
+  form#posts-filter table.wp-list-table thead #cb,
+  form#posts-filter table.wp-list-table thead #order_actions,
+  form#posts-filter table.wp-list-table thead #customer_message,
+  form#posts-filter table.wp-list-table thead #order_notes,
+  form#posts-filter table.wp-list-table thead #shipping_address {
+    display: none; }
+  form#posts-filter table.wp-list-table tbody .check-column,
+  form#posts-filter table.wp-list-table tbody .order_actions,
+  form#posts-filter table.wp-list-table tbody .customer_message,
+  form#posts-filter table.wp-list-table tbody .order_notes,
+  form#posts-filter table.wp-list-table tbody .shipping_address {
+    display: none; }
+  form#posts-filter table.wp-list-table tbody td.column-order_title {
+    width: 25%;
+    font-size: 13px;
+    font-weight: bold; }
+  form#posts-filter table.wp-list-table tfoot .column-cb,
+  form#posts-filter table.wp-list-table tfoot .column-order_actions,
+  form#posts-filter table.wp-list-table tfoot .column-customer-message,
+  form#posts-filter table.wp-list-table tfoot .column-order_notes,
+  form#posts-filter table.wp-list-table tfoot .column-shipping_address {
+    display: none; }
